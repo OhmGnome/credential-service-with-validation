737c7e384530a7cea0456bac5b6656fd5b449392
OhmGnome <stefandidier9@gmail.com>
2021-05-22 11:03:05 -0700

    Update and rename app.service.spec.ts to validatio.service.spec.ts

--- src/app/validatio.service.spec.ts
+++ src/app/validatio.service.spec.ts
@@ -1,44 +1,44 @@
 import { Credentials } from './model';
-import { AppService } from './app.service';
+import { ValidationService } from './validation.service';
 
 
 let credentials = {} as Credentials
 
-describe('AppService', () => {
-    let appService: AppService,
+describe('ValidationService', () => {
+    let validationService: ValidationService,
         mockHttp;
 
 
 beforeEach(() => {
     mockHttp = jasmine.createSpyObj('mockHttp', ['post'])
-    appService = new AppService(mockHttp)
+    validationService = new ValidationService(mockHttp)
 })
 
 
 describe('validate', () => {
     it('should return a variety error if the pasword does not contain at least one number', () => {
         credentials.password = 'allAlphabet'
-        expect(appService.validate(credentials)).toContain('variety')
+        expect(validationService.validate(credentials)).toContain('variety')
     })
 
     it('should return a variety error if the pasword does not contain at least one letter', () => {
         credentials.password = '12345567'
-        expect(appService.validate(credentials)).toContain('variety')
+        expect(validationService.validate(credentials)).toContain('variety')
     })
 
     it('should return a duplicates error if the pasword contains repeats', () => {
         credentials.password = 'aaaabb'
-        expect(appService.validate(credentials)).toContain('duplicates')
+        expect(validationService.validate(credentials)).toContain('duplicates')
     })
 
     it('should return a length error if the pasword is too short', () => {
         credentials.password = 'four'
-        expect(appService.validate(credentials)).toContain('length')
+        expect(validationService.validate(credentials)).toContain('length')
     })
 
     it('should return a length error if the pasword is too long', () => {
         credentials.password = 'fourfourfour1234567890'
-        expect(appService.validate(credentials)).toContain('length')
+        expect(validationService.validate(credentials)).toContain('length')
     })
 })
 
